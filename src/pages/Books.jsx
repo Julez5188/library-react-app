981bb7f450977010c3f30c49354595179047f690
Julia Maximov <julia.maximov18@gmail.com>
2026-09-25 07:20:02 -0400

    bug fix

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -10,7 +10,7 @@ const Books = () => {
               <div className="books__header">
                 <h2 className="section__title books__header--title">All Books</h2>
                 <select id="filter">
-                  <option value="" selected disabled>Sort</section>
+                  <option value="" selected disabled>Sort</option>
                   <option value="LOW_TO_HIGH">Price, Low to High</option>
                   <option value="HIGH_TO_LOW">Price, High to Low</option>
                   <option value="RATING">Rating</option>
