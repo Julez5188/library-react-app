b53e963e55cfc2811a185adb2e4faf653c7a1181
Julia Maximov <julia.maximov18@gmail.com>
2026-09-24 23:08:26 -0400

    added landing section and highlights section 9/24/26

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,14 +16,42 @@ const Nav = () => {
             </a>
           </li>
            <li className="nav__list">
-            <a href="/" className="nav__link">
+            <a href="/books" className="nav__link">
               Books
             </a>
           </li>
           <button className="btn__menu">
             <FontAwesomeIcon icon="bars" />
           </button>
+          <li className="Nav__icon">
+            <a href="/cart" className="nav__link">
+              <FontAwesomeIcon icon="shopping-cart" />
+            </a>
+            <span className="cart__length">3</span>
+          </li>
         </ul>
+        <div className="menu__backdrop">
+          <button className="btn__menu btn__menu--close">
+            <FontAwesomeIcon icon="times" />
+          </button>
+          <ul className="menu__links">
+            <li className="menu__list">
+              <a href="/" className="menu__link">
+                Home
+              </a>
+            </li>
+            <li className="menu__list">
+              <a href="/books" className="menu__link">
+                Books
+              </a>
+            </li>
+            <li className="menu__list">
+              <a href="/cart" className="menu__link">
+                Cart
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
