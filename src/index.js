07727e04cb7ae1b94c2abb401800615731cb7114
Julia Maximov <julia.maximov18@gmail.com>
2026-09-24 21:33:40 -0400

    bug fixes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faBars)
 
-const root = ReactDOM.createRoot(document.getElementById(root));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
