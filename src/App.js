9f30e429b1d31e648c9277ba99abb3500effdd4d
Julia Maximov <julia.maximov18@gmail.com>
2026-09-25 00:54:29 -0400

    added books section need to do bug fixes on it tomorrow

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './index.css';
 import Nav from './components/Nav';
 import Landing from './components/Landing';
 import Highlights from './components/Highlights';
+import Features from './components/Features';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Nav />
       <Landing />
       <Highlights /> 
+      <Features />
     </div>
   );
 }
---
+++ src/components/Features.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Book from "./Ui/Book";
+import { books } from "../data";
+
+const Features = () => {
+  console.log(books);
+  console.log();
+  return (
+    <section id="features">
+      <div className="container">
+        <div className="row">
+          <h2 className="section__title">
+            Featured <span className="orange">Books</span>
+          </h2>
+          <div className="books">
+            {books
+            .filter(book => book.rating === 5)
+            .slice(0,4)
+            .map((book) => {
+              <Book book={book} key={book.id}/>
+            })}
+            
+            <Book />
+            <Book />
+            <Book />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
